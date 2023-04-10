8ab4ede8d921d23669f3d9972c071f654420d68a
Darina Fedoseeva <darina.fedoseeva@rtlabs.ru>
2023-04-10 10:09:39 +0300

    cleared main file

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,45 +1,8 @@
 package jm.task.core.jdbc;
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-
-import java.sql.*;
 
 public class Main {
-
-
     public static void main(String[] args) {
-        UserDao userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.createUsersTable();
 
-
-        /*System.out.println("Testing connection to PostgreSQL JDBC");
-
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
-            e.printStackTrace();
-            return;
-        }
-
-        System.out.println("PostgreSQL JDBC Driver successfully connected");
-        Connection connection = null;
-
-        try {
-            connection = DriverManager
-                    .getConnection(DB_URL, USER, PASS);
-
-        } catch (SQLException e) {
-            System.out.println("Connection Failed");
-            e.printStackTrace();
-            return;
-        }
-
-        if (connection != null) {
-            System.out.println("You successfully connected to database now");
-        } else {
-            System.out.println("Failed to make connection to database");
-        }*/
     }
 }
 
